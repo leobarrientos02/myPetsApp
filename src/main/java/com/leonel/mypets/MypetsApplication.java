bfad02ad8008a66d42e642b421d2ca2243f69ccb
Your Name <leobarrientos02@gmail.com>
2022-05-05 10:34:10 -0400

    boiler plate

---
+++ src/main/java/com/leonel/mypets/MypetsApplication.java
@@ -0,0 +1,13 @@
+package com.leonel.mypets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetsApplication.class, args);
+	}
+
+}
