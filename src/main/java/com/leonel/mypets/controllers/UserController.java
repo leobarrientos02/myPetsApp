68c8e6081df24621d047ff5ae920122d12440b1e
leobarrientos02 <leobarrientos02@gmail.com>
2022-05-11 02:49:50 -0400

    servlets

--- src/main/java/com/leonel/mypets/controllers/UserController.java
+++ src/main/java/com/leonel/mypets/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.leonel.mypets.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @GetMapping("/auth")
+    @ResponseStatus()
 }
